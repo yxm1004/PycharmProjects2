fb62c6f20c158564c659af2a60042b09a9dca7c3
yxm1004 <1310277501@qq.com>
2023-03-07 19:44:13 +0800

    提交

--- interfaceTest/testCase/mycase/testcomponentsave.py
+++ interfaceTest/testCase/mycase/testcomponentsave.py
@@ -2,10 +2,12 @@ import unittest
 import requests
 import json
 
+from interfaceTest.common.ApiConstants import ApiConstants
 from interfaceTest.common.GetToken import Token
 from interfaceTest import readConfig
 
 localReadConfig = readConfig.ReadConfig()
+
 
 class testcomponentsave(unittest.TestCase):
     def setUp(self):
@@ -14,32 +16,37 @@ class testcomponentsave(unittest.TestCase):
         tk = Token()
         self.token = 'Bearer ' + tk.get_token()
         print("self.token-------", self.token)
-        payload = json.dumps({
-        "bomDetailList": [],
-        "rebarWeight": "30.475",
-        "projectId": "434",
-        "building": "6",
-        "floorSegment": "3",
-        "type": "叠合板",
-        "model": "DHB003",
-        "floorCount": "1",
-        "appearanceSizeLength": "2500",
-        "appearanceSizeWidth": "2320",
-        "appearanceSizeThickness": "60",
-        "squareAmount": "0.636",
-        "weight": "0.255",
-        "concreteLabel": "C30",
-        "concreteDosage": "0.636"
-    })
-    headers = {
+
+        #公告头部
+        self.headers = {
         'Authorization': 'Basic Y2xvdWRmYWN0b3J5X3dlYjpjbG91ZGZhY3Rvcnlfd2ViX3NlY3JldA==',
         'tenant': 'ZGdnYw==',
-        'token': self.token ,
+        'token': self.token,
         'Content-Type': 'application/json'
     }
+    #测试用例
+    def test_post_components(self):
 
-    response = requests.request("POST",self.constants.componentsave_url, headers=headers, data=payload)
-    self.assertEqual(response.status_code, 200)
+        payload = json.dumps({
+            "bomDetailList": [],
+            "rebarWeight": "30.475",
+            "projectId": "434",
+            "building": "6",
+            "floorSegment": "3",
+            "type": "叠合板",
+            "model": "DHB003",
+            "floorCount": "1",
+            "appearanceSizeLength": "2500",
+            "appearanceSizeWidth": "2320",
+            "appearanceSizeThickness": "60",
+            "squareAmount": "0.636",
+            "weight": "0.255",
+            "concreteLabel": "C30",
+            "concreteDosage": "0.636"
+        })
+        response = requests.request("POST", self.constants.componentsave_url, headers=self.headers, data=payload)
+        self.assertEqual(response.status_code, 200)
+
+
 if __name__ == '__main__':
-    main()
-
+    unittest.main()#单元测试

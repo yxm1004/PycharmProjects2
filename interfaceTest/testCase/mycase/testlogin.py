8e32ab3f56c57b0da8b96a04f846cb3290675792
duanwq666 <619485922@qq.com>
2023-03-07 16:57:26 +0800

    常量类

--- interfaceTest/testCase/mycase/testlogin.py
+++ interfaceTest/testCase/mycase/testlogin.py
@@ -1,17 +1,20 @@
 import json
 import unittest
 import requests
-from interfaceTest.common import ApiConstants
+from interfaceTest.common.ApiConstants import ApiConstants
 from interfaceTest import readConfig
 
 localReadConfig = readConfig.ReadConfig()
-#1、写一个类继承unitest会自动引入的
+
+
+# 1、写一个类继承unitest会自动引入的
 class testlogin(unittest.TestCase):
-    #2、定义一个测试方法test_开头
+    # 2、定义一个测试方法test_开头
 
     def setUp(self):
-        #引入常量类，直接使用常量类中的url地址
+        # 引入常量类，直接使用常量类中的url地址
         self.constants = ApiConstants()
+        self.url = self.constants.BASE_URL
         self.account = localReadConfig.get_OPTION("account")
         self.password = localReadConfig.get_OPTION("password")
         self.headers = {
@@ -20,12 +23,13 @@ class testlogin(unittest.TestCase):
             'token': 'Bearer test',
             'Content-Type': 'application/json'
         }
+
     def test_post_login(self):
-        #3、拷贝postman中code
-        #获取baseurl
+        # 3、拷贝postman中code
+        # 获取baseurl
         # baseurl = localReadConfig.get_http("baseurl")
         # url = baseurl+"/api/oauth/noToken/login"
-        #获取登录账号
+        # 获取登录账号
         # account = localReadConfig.get_OPTION("account")
         # password = localReadConfig.get_OPTION("password")
         payload = json.dumps({
@@ -40,15 +44,16 @@ class testlogin(unittest.TestCase):
             "refreshToken": ""
         })
 
-
-        response = requests.request("POST", url, headers=self.headers, data=payload)
-        if response.status_code==200:
+        response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        if response.status_code == 200:
             print(response.json()["data"]["token"])
             return response.json()["data"]["token"]
         else:
             print("failed to get token")
-        #4.写一个断言
+        # 4.写一个断言
         self.assertEqual(response.status_code, 200)
-#5.验证这个用例
+
+
+# 5.验证这个用例
 if __name__ == '__main__':
     unittest.main()

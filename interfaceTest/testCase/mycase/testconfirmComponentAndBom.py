51cb376b2b6834412c3a0c80229c969acd8bf551
yxm1004 <1310277501@qq.com>
2023-03-07 22:44:30 +0800

    提交

--- interfaceTest/testCase/mycase/testconfirmComponentAndBom.py
+++ interfaceTest/testCase/mycase/testconfirmComponentAndBom.py
@@ -42,8 +42,10 @@ class confirmComponentAndBom(unittest.TestCase):
             "concreteLabel": "C30",
             "concreteDosage": "0.636"
         })
+        #保存构件信息
         response = requests.request("POST", self.constants.COMPONENTSAVE_URL, headers=self.headers, data=payload)
         data = json.loads(response.text)
+        #提取strKey
         self.strKey = data["data"]["strKey"]
         print(self.strKey)
 
@@ -51,8 +53,12 @@ class confirmComponentAndBom(unittest.TestCase):
     # 测试用例get类型测试用例
     def test_get_confirmComponentAndBom(self):
         print("queren", self.strKey)
-        response = requests.request("GET", self.constants.CONFIRMCOMPONENTANDBOM_URL, params=self.strKey,
+        #初始化get参数
+        params={"key":self.strKey}
+        response = requests.request("GET", self.constants.CONFIRMCOMPONENTANDBOM_URL, params=params,
                                     headers=self.headers)
+        data = json.loads(response.text)
+        print(data)
         self.assertEqual(response.status_code, 200)
 
 

75e3d69a9e131f2782732497c868da73e3ada9d9
yxm1004 <1310277501@qq.com>
2023-03-07 19:31:27 +0800

    修改错误

--- interfaceTest/testCase/mycase/testcomponentsave.py
+++ interfaceTest/testCase/mycase/testcomponentsave.py
@@ -8,18 +8,12 @@ from interfaceTest import readConfig
 localReadConfig = readConfig.ReadConfig()
 
 class testcomponentsave(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        # 获取token
-        tk = Token()
-        self.token = 'Bearer ' + tk.get_token()
-        print("self.token-------", self.token)
-
     def setUp(self):
         # 引入常量类，直接使用常量类中的url地址
         self.constants = ApiConstants()
-
-    ayload = json.dumps({
+        self.account = localReadConfig.get_OPTION("account")
+        self.password = localReadConfig.get_OPTION("password")
+        payload = json.dumps({
         "bomDetailList": [],
         "rebarWeight": "30.475",
         "projectId": "434",
@@ -39,11 +33,11 @@ class testcomponentsave(unittest.TestCase):
     headers = {
         'Authorization': 'Basic Y2xvdWRmYWN0b3J5X3dlYjpjbG91ZGZhY3Rvcnlfd2ViX3NlY3JldA==',
         'tenant': 'ZGdnYw==',
-        'token': 'self.token' ,
+        'token': self.token ,
         'Content-Type': 'application/json'
     }
 
-    response = requests.request("POST", self.constants.componentsave_url, headers=headers, data=payload)
+    response = requests.request("POST",self.constants.componentsave_url, headers=headers, data=payload)
     self.assertEqual(response.status_code, 200)
 if __name__ == '__main__':
     main()

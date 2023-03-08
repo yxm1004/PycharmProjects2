4331c92bd4cf87c5be25acb5b0aa5477d5699465
yxm1004 <1310277501@qq.com>
2023-03-08 20:43:14 +0800

    提交

--- interfaceTest/requestsApi/saveComponentAndBomApi.py
+++ interfaceTest/requestsApi/saveComponentAndBomApi.py
@@ -5,7 +5,6 @@ import json
 from interfaceTest.common.ApiConstants import ApiConstants
 from interfaceTest.common.GetToken import Token
 from interfaceTest import readConfig
-from interfaceTest.testCase.mycase.testcomponentsave import testcomponentsave
 
 localReadConfig = readConfig.ReadConfig()
 

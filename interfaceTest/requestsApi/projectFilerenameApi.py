0863986449f2b881ca03c78d254f77b3d8333011
yxm1004 <1310277501@qq.com>
2023-03-09 19:29:18 +0800

    提交

---
+++ interfaceTest/requestsApi/projectFilerenameApi.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+from interfaceTest import readConfig
+from interfaceTest.requestsApi.loginApi import loginApi
+
+localReadConfig = readConfig.ReadConfig()
+class projectrFilerenameApi():
+    def __init__(self):
+        baseurl = localReadConfig.get_http("baseurl")
+        self.url = baseurl + "/api/report/deliveryPlan"
+        # 初始化头部
+        lg = loginApi()
+        # 从登陆类里获取头部
+        self.headers = lg.getheaders()
+    def projectrFilerename(self):
+        payload = json.dumps({
+            "fileId": 1632951176049876992,
+            "name": "香江悦府16",
+            "phone": "",
+            "reportUserId": 0
+        })
+        response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        return response
+if __name__ == '__main__':
+    pc = projectrFilerenameApi
+    response=pc.projectrFilerename()
+    print(response.json() )
+

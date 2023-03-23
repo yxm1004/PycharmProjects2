ae5209ef3612f211fba4a0827b488a9fd62d341b
yxm1004 <1310277501@qq.com>
2023-03-23 14:46:36 +0800

    提交

--- interfaceTest/requestsApi/materialPleaseOrderAPI.py
+++ interfaceTest/requestsApi/materialPleaseOrderAPI.py
@@ -5,4 +5,33 @@ from interfaceTest import readConfig
 from interfaceTest.requestsApi.loginApi import loginApi
 
 localReadConfig = readConfig.ReadConfig()
-class materialPleaseOrder
+class materialPleaseOrderApi():
+      def __init__(self):
+        baseurl = localReadConfig.get_http("baseurl")
+        self.url = baseurl + "/api/report/deliveryPlan"
+        # 初始化头部
+        lg = loginApi()
+        #从登陆类里获取头部
+        self.headers = lg.getheaders()
+      def materialPleaseOrder(self):
+          """
+                     新增物料请购单-新增
+          """
+          payload = json.dumps([
+              {
+                  "building": "6",
+                  "floor": "3",
+                  "floorLevels": 1,
+                  "projectId": 434,
+                  "requiredTime": "2023-03-10"
+              }
+          ])
+          response = requests.request("POST", self.url, headers=self.headers, data=payload)
+          return response
+if __name__ == '__main__':
+    pc = materialPleaseOrderApi()
+    response=pc.materialPleaseOrder()
+    print(response.json())
+
+
+

560db845f6d63e1ac65426c24fed6b55a4b36a3f
yxm1004 <1310277501@qq.com>
2023-03-24 16:22:28 +0800

    提交

--- interfaceTest/requestsApi/accountStatementqueryApi.py
+++ interfaceTest/requestsApi/accountStatementqueryApi.py
@@ -5,12 +5,29 @@ from interfaceTest import readConfig
 from interfaceTest.requestsApi.loginApi import loginApi
 
 localReadConfig = readConfig.ReadConfig()
-class ccountStatementqueryApi():
+class accountStatementqueryApi():
     def __init__(self):
         baseurl = localReadConfig.get_http("baseurl")
-        self.url = baseurl + "/api/report/subcontractingStatementRules/updateSubcontracting"
+        self.url = baseurl + "/api/report/accountStatement/queryPage"
         # 初始化头部
         lg = loginApi()
         # 从登陆类里获取头部
         self.headers = lg.getheaders()
-    def
+    def accountStatemenntquery(self):
+        """
+                   对账单-自定义分页查询
+        """
+        payload = json.dumps({
+            "model": {
+                "list": []
+            },
+            "extra": {},
+            "current": 1,
+            "size": 50
+        })
+        response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        return response
+if __name__ == '__main__':
+    pc = accountStatementqueryApi()
+    response = pc.accountStatemenntquery()
+    print(response.json())

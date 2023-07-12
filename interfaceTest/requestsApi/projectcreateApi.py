5699f58d72bac47df157de42d29e4245100a9a6f
yxm1004 <1310277501@qq.com>
2023-07-12 10:53:52 +0800

    提交

--- interfaceTest/requestsApi/projectcreateApi.py
+++ interfaceTest/requestsApi/projectcreateApi.py
@@ -17,33 +17,34 @@ class projectcreatApi:
         self.headers = lg.getheaders()
     def projectcreat(self,abbreviation):
         self.payload = json.dumps({
-            "name": "",
-            "orderType": "大乐装（东莞）建筑科技有限公司",
+            "name": "验证",
+            "orderType": "大乐装（惠州）建筑科技有限公司",
             "abbreviation": abbreviation,
-            "contractNo": "",
-            "province": 1,
-            "city": 2,
-            "region": 6,
+            "contractNo": "ww-202307122",
+            "province": 576,
+            "city": 599,
+            "region": 600,
             "address": [
-                1,
-                2,
-                6
+                576,
+                599,
+                600
             ],
-            "addressDetail": "",
+            "addressDetail": "铁西建筑局01",
             "note": "",
             "manufacturer": "大乐装 | 数字化交付平台",
-            "contactPerson": "",
-            "contactPhone": "",
-            "salePerson": "尹卡卡",
+            "contactPerson": "尹123",
+            "contactPhone": "158 7965 4576",
+            "salePerson": "尹晓梅",
             "salePhone": "153 1348 7958",
-            "clientName": "",
-            "contractBelong": "",
+            "clientName": "铁西建筑局",
+            "contractBelong": "大乐装01",
             "customers": [],
             "projectType": 1,
             "allocationType": None,
             "coordinationFactory": None,
             "allocationList": [],
-            "logo": None
+            "logo": None,
+            "orderSource": 2
         })
         response = requests.request("POST", self.url, headers=self.headers, data=self.payload)
         return response
@@ -51,5 +52,5 @@ class projectcreatApi:
 
 if __name__ == '__main__':
     pc = projectcreatApi()
-    rs = pc.projectcreat("你好0627号12")
+    rs = pc.projectcreat("你好07121050号")
     print(rs.json())

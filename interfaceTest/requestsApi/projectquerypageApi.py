732239b3f91a3e343c7dc0bc05318dd13afa6c61
yxm1004 <1310277501@qq.com>
2023-07-12 11:41:46 +0800

    提交

---
+++ interfaceTest/requestsApi/projectquerypageApi.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+from interfaceTest import readConfig
+from interfaceTest.requestsApi.loginApi import loginApi
+
+localReadConfig = readConfig.ReadConfig
+class projectquerypageApi():
+    def __init__(self):
+        baseurl = localReadConfig.get_http("baseurl")
+        self.url = baseurl + "/api/report/project/query/page"
+        # 初始化头部
+        lg = loginApi()
+        # 从登陆类里获取头部
+        self.headers = lg.getheaders()
+    def projectquerypage(self):
+        """
+                分页查询项目列表
+        """
+        payload = json.dumps({
+            "model": {
+                "list": [],
+                "projectStatus": "1"
+            },
+            "extra": {},
+            "current": 1,
+            "size": 50
+        })
+        response = requests.request("post", self.url, headers=self.headers, data=payload)
+        return response
+if __name__ == '__main__':
+    pc = projectquerypageApi()
+    response = pc.projectquerypage()
+    print(response.json())

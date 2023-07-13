82d06335808dc92a2f8d51564a64d4fbdf585b7e
yxm1004 <1310277501@qq.com>
2023-07-13 14:57:37 +0800

    提交

---
+++ interfaceTest/test_dir/testprocessPlan.py
@@ -0,0 +1,31 @@
+import json
+import datetime
+import time
+
+import seldom
+from common.loginApi import Common
+
+
+class processPlan(seldom.TestCase):
+    def start(self):
+        #调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+
+    def test_put_processPlan(self):
+        self.payload = json.dumps({
+            "id": "1679318602571841536",
+            "productionRounds": "2",
+            "schemeNumber": "2",
+            "schemeSequence": "2",
+            "schemeCarNumber": "2",
+            "operation": "0"
+        })
+
+
+        self.put("/api/report/processPlan", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+
+
+if __name__ == '__main__':
+     seldom.main(debug=True)

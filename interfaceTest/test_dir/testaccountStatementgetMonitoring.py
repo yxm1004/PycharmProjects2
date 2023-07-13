6188e6791e543ecbcf51b2a3e912a52661d1a7e6
yxm1004 <1310277501@qq.com>
2023-07-13 20:18:45 +0800

    提交

--- interfaceTest/test_dir/testaccountStatementgetMonitoring.py
+++ interfaceTest/test_dir/testaccountStatementgetMonitoring.py
@@ -9,14 +9,14 @@ class testaccountStatementgetMonitoring(seldom.TestCase):
         # 调用登录公共方法构建报文头
         self.c = Common()
         self.header = self.c.SetHeader()
-    def accountStatementgetMonitoring(self):
+    def test_post_accountStatementgetMonitoring(self):
         """
                 对账回款监测
         """
         self.payload = json.dumps({
             "screen": 0,
-            "endTime": "2023-05-31",
-            "startTime": "2023-05-01"
+            "endTime": "2023-06-31",
+            "startTime": "2023-06-01"
         })
         self.post("/api/report/accountStatement/getMonitoring", data=self.payload, headers=self.header)
         self.assertStatusCode(200)
@@ -24,3 +24,4 @@ class testaccountStatementgetMonitoring(seldom.TestCase):
 
 if __name__ == '__main__':
     seldom.main(debug=True)
+

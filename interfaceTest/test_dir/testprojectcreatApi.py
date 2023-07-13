55a30e3b4d3282dc708573ae5663db3fa4b5a4bc
duanwq <619485922@qq.com>
2023-07-13 11:24:58 +0800

    1、封装登录公告方法 2、新增创建项目case

--- interfaceTest/test_dir/testprojectcreatApi.py
+++ interfaceTest/test_dir/testprojectcreatApi.py
@@ -8,6 +8,7 @@ from common.loginApi import Common
 
 class testprojectcreat(seldom.TestCase):
     def start(self):
+        #调用登录公共方法构建报文头
         self.c = Common()
         self.header = self.c.SetHeader()
     def test_post_projectcreat(self):

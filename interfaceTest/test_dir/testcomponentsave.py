9757c81b4baefdd47eb0fa1110c0ed882a3d6062
86131 <619485922@qq.com>
2023-07-13 22:39:45 +0800

    用例依赖

--- interfaceTest/test_dir/testcomponentsave.py
+++ interfaceTest/test_dir/testcomponentsave.py
@@ -1,25 +1,32 @@
 import json
 import seldom
+from seldom import depend
 
 from common.loginApi import Common
-from interfaceTest import readConfig
-
-localReadConfig = readConfig.ReadConfig()
 
 
 class testcomponentsave(seldom.TestCase):
-    def start(self):
-        #调用登录公共方法构建报文头
+    global strKey
+
+    @classmethod
+    def start_class(self):
+        # 调用登录公共方法构建报文头
+        print("执行次数")
         self.c = Common()
         self.header = self.c.SetHeader()
 
-    #测试用例
-    def test_post_componentsave(self):
+    def test_002_confirmComponentAndBom(self):
+        print("---------------" + globals()['strKey'])
+        params = {"key": globals()['strKey']}
+        self.get("/api/report/component/confirmComponentAndBom", params=params, headers=self.header)
+        self.assertStatusCode(200)
 
-        payload = json.dumps({
+    # 测试用例
+    def test_001_componentsave(self):
+        self.payload = json.dumps({
             "bomDetailList": [],
             "rebarWeight": "30.475",
-            "projectId": "434",
+            "projectId": "1039",
             "building": "6",
             "floorSegment": "3",
             "type": "叠合板",
@@ -33,7 +40,9 @@ class testcomponentsave(seldom.TestCase):
             "concreteLabel": "C30",
             "concreteDosage": "0.636"
         })
-        self.post("POST", "/api/report/component/saveComponentAndBom", headers=self.headers, data=payload)
+        self.post("/api/report/component/saveComponentAndBom", data=self.payload, headers=self.header)
+        globals()['strKey']=json.dumps(self.response['data']['strKey'])
+        print(globals()['strKey']+"22222222222222222222222")
         self.assertStatusCode(200)
 
 

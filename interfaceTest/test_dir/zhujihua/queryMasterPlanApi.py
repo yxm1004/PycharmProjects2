96fe838291381077d6c199a1cac060eadf37aa8d
yxm1004 <1310277501@qq.com>
2023-07-24 11:31:27 +0800

    提交

---
+++ interfaceTest/test_dir/zhujihua/queryMasterPlanApi.py
@@ -0,0 +1,27 @@
+import json
+import datetime
+import time
+
+import seldom
+from common.loginApi import Common
+class testqueryMasterPlan(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_queryMasterPlanApi(self):
+        """
+                   查询主计划
+        """
+        payload = json.dumps({
+            "createTime": "2023-06",
+            "orgIds": [],
+            "projectIds": [],
+            "version": 0
+        })
+        self.post("/api/report/masterPlan/queryMasterPlan", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

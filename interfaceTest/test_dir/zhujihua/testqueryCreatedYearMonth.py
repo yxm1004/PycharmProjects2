9c2b55d900e72e227ff042ac9d2784c858602afe
yxm1004 <1310277501@qq.com>
2023-07-24 11:23:10 +0800

    提交

--- interfaceTest/test_dir/zhujihua/testqueryCreatedYearMonth.py
+++ interfaceTest/test_dir/zhujihua/testqueryCreatedYearMonth.py
@@ -5,19 +5,19 @@ import time
 import seldom
 from common.loginApi import Common
 
-class testcreatedYearMonth(seldom.TestCase):
+class testquerycreatedYearMonth(seldom.TestCase):
     def start(self):
         # 调用登录公共方法构建报文头
         self.c = Common()
         self.header = self.c.SetHeader()
-    def test_get_createdYearMonth(self):
+    def test_get_querycreatedYearMonth(self):
         """
-                对账单-列表查询
+                查询已经创建主计划的年月份
         """
         self.payload = {}
-        self.get("/api/report/masterPlan/versionList?createdYearMonth=2023-06", data=self.payload, headers=self.header)
+        self.get("/api/report/masterPlan/queryCreatedYearMonth", data=self.payload, headers=self.header)
         self.assertStatusCode(200)
-        self.assertEqual(msg="成功")
+
 
 
 if __name__ == '__main__':

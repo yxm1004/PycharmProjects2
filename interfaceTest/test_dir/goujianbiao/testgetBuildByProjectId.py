a415f6b8bad9ce47c9aa337f6435e38e37892aa0
yxm1004 <1310277501@qq.com>
2023-07-25 18:23:24 +0800

    文件提交

---
+++ interfaceTest/test_dir/goujianbiao/testgetBuildByProjectId.py
@@ -0,0 +1,28 @@
+import json
+import datetime
+import time
+
+import seldom
+from common.loginApi import Common
+
+class testgetBuildByProjectId(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_get_getBuildByProjectId(self):
+        """
+            根据项目id获取楼栋信息
+        """
+        self.payload = {}
+        self.post("/api/report/component/listByPage", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

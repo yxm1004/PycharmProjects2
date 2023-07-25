25ea70eb323be3f8624d764394c296a458f20f56
yxm1004 <1310277501@qq.com>
2023-07-25 11:27:12 +0800

    文件提交

---
+++ interfaceTest/test_dir/kaipiaodengji/testqueryInvoicingEntryDetail.py
@@ -0,0 +1,29 @@
+import json
+import datetime
+import time
+
+import seldom
+from common.loginApi import Common
+
+class testqueryInvoicingEntryDetail(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_get_queryInvoicingEntryDetail(self):
+        """
+                获取开票入账详情
+        """
+        self.payload = {}
+        self.get("/api/report/invoicingRecord/queryInvoicingEntryDetail?id=1682231123687505920", data=self.payload,
+                  headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

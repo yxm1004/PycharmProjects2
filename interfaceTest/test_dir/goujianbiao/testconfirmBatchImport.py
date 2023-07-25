0fe5aea9a089ea39c46027057e8fa76299d69d2e
yxm1004 <1310277501@qq.com>
2023-07-25 18:11:14 +0800

    文件提交

---
+++ interfaceTest/test_dir/goujianbiao/testconfirmBatchImport.py
@@ -0,0 +1,28 @@
+import json
+import datetime
+import time
+
+import seldom
+from common.loginApi import Common
+
+class testconfirmBatchImport(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_confirmBatchImport(self):
+        """
+            确认导入构件
+        """
+        self.payload = {}
+        self.post("/api/report/component/confirmBatchImport", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

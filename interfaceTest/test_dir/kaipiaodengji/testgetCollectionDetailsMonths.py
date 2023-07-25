cbe40a3155e6fa8b73f7381797083811350cf068
yxm1004 <1310277501@qq.com>
2023-07-25 11:11:38 +0800

    文件提交

---
+++ interfaceTest/test_dir/kaipiaodengji/testgetCollectionDetailsMonths.py
@@ -0,0 +1,33 @@
+import json
+import datetime
+import time
+
+import seldom
+from common.loginApi import Common
+class testgetCollectionDetailsMonths(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_getCollectionDetailsMonths(self):
+        """
+                开票登记-查询开票账期月份
+        """
+        self.payload = json.dumps({
+            "dataWay": 2,
+            "projectId": "1063",
+            "type": "1"
+        })
+        self.post("/api/report/invoicingRecord/getCollectionDetailsMonths", data=self.payload,
+                  headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)
+

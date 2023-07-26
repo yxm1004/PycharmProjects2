c95e17085686ec6ff769a8e821b5e9296e113f9e
yxm1004 <1310277501@qq.com>
2023-07-26 20:17:05 +0800

    文件提交

---
+++ interfaceTest/test_dir/xiangmugunali/testprojectquerypage.py
@@ -0,0 +1,34 @@
+import json
+import seldom
+from common.loginApi import Common
+class testprojectquerypage(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_projectquerypage(self):
+        """
+             分页查询项目列表
+        """
+        self.payload = json.dumps({
+            "model": {
+                "list": [],
+                "projectStatus": "1"
+            },
+            "extra": {},
+            "current": 1,
+            "size": 50
+        })
+        self.post("/api/report/project/query/page", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)
+
+

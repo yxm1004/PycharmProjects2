45e26e9f0c2383bd1c291376a6439b20e4c0265f
yxm1004 <1310277501@qq.com>
2023-07-26 11:29:20 +0800

    文件提交

---
+++ interfaceTest/test_dir/goujianbiao/testconfirmDeleteComponent.py
@@ -0,0 +1,41 @@
+import json
+import datetime
+import time
+
+import seldom
+from common.loginApi import Common
+
+class testconfirmDeleteComponent(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_001_deleteComponentByIds(self):
+        """
+            删除构件
+        """
+        self.payload = json.dumps({
+            "componentParentIds": [],
+            "componentIds": [
+                "1684033006378942466"
+            ]
+        })
+        self.post("/api/report/component/deleteComponentByIds", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        self.assertJSON(True, self.response["isSuccess"])
+    def test_002_confirmDeleteComponent(self):
+        """
+                确认删除构件
+        """
+        payload = json.dumps({
+            "componentParentIds": [],
+            "componentIds": [
+                "1684033006378942466"
+            ]
+        })
+        self.post("/api/report/component/confirmDeleteComponent", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        self.assertJSON(True, self.response["isSuccess"])
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

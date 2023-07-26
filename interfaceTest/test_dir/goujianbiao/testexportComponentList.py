f1ef927463319a388b2ddc9e86955fde8754ecfa
yxm1004 <1310277501@qq.com>
2023-07-26 13:44:18 +0800

    文件提交

---
+++ interfaceTest/test_dir/goujianbiao/testexportComponentList.py
@@ -0,0 +1,25 @@
+import json
+import datetime
+import time
+
+import seldom
+from common.loginApi import Common
+class testexportComponentList(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_exportComponentList(self):
+        """
+            导出构件清单
+        """
+        self.payload = json.dumps({
+            "projectId": "1058",
+            "list": []
+        })
+        self.post("/api/report/component/exportComponentList",data=self.payload,headers=self.header)
+        self.assertStatusCode(200)
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

569c59721e55a235a9e91850bda088b464655e49
yxm1004 <1310277501@qq.com>
2023-07-26 11:48:20 +0800

    文件提交

--- interfaceTest/test_dir/goujianbiao/testdeleteComponentByIds.py
+++ interfaceTest/test_dir/goujianbiao/testdeleteComponentByIds.py
@@ -9,22 +9,32 @@ class testdeleteComponentByIds(seldom.TestCase):
         # 调用登录公共方法构建报文头
         self.c = Common()
         self.header = self.c.SetHeader()
-    def test_post_deleteComponentByIds(self):
+    def test_001_deleteComponentByIds(self):
         """
             删除构件
         """
         self.payload = json.dumps({
             "componentParentIds": [],
             "componentIds": [
-                "1684033006378942466"
+                "1680818087357579264"
             ]
         })
         self.post("/api/report/component/deleteComponentByIds", data=self.payload,headers=self.header)
         self.assertStatusCode(200)
-        assert_data = "成功"  # 断言成功
-        # print("test-----------------"+self.response["msg"])
-        # 取返回msg值断言
-        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+    def test_002_confirmDeleteComponent(self):
+        """
+            确认删除构件
+        """
+        self.payload = json.dumps({
+            "componentParentIds": [],
+            "componentIds": [
+                "1680818087357579264"
+            ]
+        })
+        self.post("/api/report/component/confirmDeleteComponent", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
         self.assertJSON(True, self.response["isSuccess"])
 
 

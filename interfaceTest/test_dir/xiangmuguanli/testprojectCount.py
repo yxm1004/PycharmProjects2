986d36e2f23bc110d711dcca191f6d9003b87ef6
yxm1004 <1310277501@qq.com>
2023-07-27 16:46:43 +0800

    文件提交

---
+++ interfaceTest/test_dir/xiangmuguanli/testprojectCount.py
@@ -0,0 +1,24 @@
+import json
+import seldom
+from common.loginApi import Common
+class testprojectCount(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_get_projectCount(self):
+        """
+           项目订单列表统计
+        """
+        self.payload = {}
+        self.get("/api/report/project/projectCount", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

67e72c8ee21e0418d5d4caee2403b4413b5c61f9
yxm1004 <1310277501@qq.com>
2023-07-27 20:22:07 +0800

    文件提交

---
+++ interfaceTest/test_dir/wuliaomingxi/testmaterialClassificationquery.py
@@ -0,0 +1,24 @@
+import json
+import seldom
+from common.loginApi import Common
+class testmaterialClassificationquery(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_materialClassificationquery(self):
+        """
+           物料分类-批量查询
+        """
+        self.payload = json.dumps({})
+        self.post("/api/report/materialClassification/query", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

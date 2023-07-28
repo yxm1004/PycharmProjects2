c697ffb4c05e7b018d725cb859b96d488fbaedbd
yxm1004 <1310277501@qq.com>
2023-07-28 14:34:25 +0800

    文件提交

--- interfaceTest/test_dir/mujuzu/testpageMoldGroup.py
+++ interfaceTest/test_dir/mujuzu/testpageMoldGroup.py
@@ -10,3 +10,21 @@ class testpageMoldGroup(seldom.TestCase):
         """
            模具组分页列表
         """
+        self.payload = json.dumps({
+            "current": 1,
+            "model": {},
+            "size": 20
+        })
+        self.post("/api/report/moldGroup/pageMoldGroup", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)
+
+

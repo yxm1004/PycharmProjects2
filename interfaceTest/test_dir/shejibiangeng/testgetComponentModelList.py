3a4d60d4244b66bbc458d91baf33c6f43b6a0b06
yxm1004 <1310277501@qq.com>
2023-07-31 15:51:58 +0800

    文件提交

---
+++ interfaceTest/test_dir/shejibiangeng/testgetComponentModelList.py
@@ -0,0 +1,25 @@
+import json
+import seldom
+from common.loginApi import Common
+class testgetComponentModelList(seldom.TestCase):
+    def start(self):
+        #调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_get_getComponentModelList(self):
+        """
+            设计变更-根据项目id获取有关联图纸的编号(去重)
+        """
+        self.payload = {}
+        self.get("/api/report/changeParent/getComponentModelList?projectId=1063", data=self.payload,
+                 headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

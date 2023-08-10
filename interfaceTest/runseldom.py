f0f2dd9a3b139c01dc454165813e1ba1158f351e
duanwq <619485922@qq.com>
2023-08-10 14:29:32 +0800

    增加微信消息推送功能

--- interfaceTest/runseldom.py
+++ interfaceTest/runseldom.py
@@ -1,10 +1,21 @@
 import seldom
+from XTestRunner import Weinxin
 
 
 class YouTest:
     """执行test_dir下所有test文件并输出测试报告"""
     if __name__ == '__main__':
             # 指定运行其他目录&文件
-            print('222222222222')
             # 运行当前文件中的用例
-            seldom.main(path='./test_dir')  # 默认运行当前文件中所有用例
+            seldom.main(path='./test_dir',
+                        base_url='https://jf-api-test-x1ksp5.dalezhuang.com',
+                        title="大乐装测试报告",
+                        tester="yinxm")  # 默认运行当前文件中所有用例
+            weixin = Weinxin(
+                # access_token="bdb12697-5c4a-4cb9-addb-49b7689f660a",  # 2817d6fc-56ad-4fa9-928a-62222c5f729b
+                access_token="2817d6fc-56ad-4fa9-928a-62222c5f729b",
+                at_mobiles=[15313487958],
+                is_at_all=True,
+            )
+            # weixin.send_text(text="\n ### 测试发送消息")
+            weixin.send_markdown(append="\n ### 附加信息")

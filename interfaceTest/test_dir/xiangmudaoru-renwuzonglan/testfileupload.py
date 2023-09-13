91a8303605ca0548ac08559b8dc47486d0c2c0ee
yxm1004 <1310277501@qq.com>
2023-09-13 15:06:03 +0800

    文件提交

---
+++ interfaceTest/test_dir/xiangmudaoru-renwuzonglan/testfileupload.py
@@ -0,0 +1,33 @@
+import seldom
+from common.loginApi import Common
+
+
+class testfileupload(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        # self.header = self.c.SetHeader()
+        #在传递文件时，不能将Content-Type头部设置为application/json，因为application/json用于表示请求体是一个JSON格式的数据，而不是文件数据
+        self.header = self.c.SetFileHeader()
+        # self.url = "https://jf-api-test-x1ksp5.dalezhuang.com/api/report/file/upload"
+
+    def test_post_fileupload(self):
+        """
+                   文件上传
+        """
+        files = [
+            ('file', ('1111.webp',
+                      open('C:\\Users\\msi\\Pictures\\1111.webp', 'rb'), 'image/webp'))
+        ]
+
+        print(self.header)
+        self.post("/api/report/file/upload", headers=self.header,  files=files)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

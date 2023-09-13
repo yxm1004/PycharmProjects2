d2b3c21fba1a14ef9bea37aba9e238874a04bc50
yxm1004 <1310277501@qq.com>
2023-09-13 15:50:26 +0800

    文件提交

---
+++ interfaceTest/test_dir/xiangmudaoru-renwuzonglan/testworkbenchTaskRecordgetModelAcceptanceInfoList.py
@@ -0,0 +1,27 @@
+import json
+import seldom
+from common.loginApi import Common
+class testworkbenchTaskRecordgetModelAcceptanceInfoList(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_workbenchTaskRecordgetModelAcceptanceInfoList(self):
+        """
+           获取模具验收卡片列表
+        """
+        self.payload = json.dumps({
+            "projectIds": [],
+            "todo": False
+        })
+        self.post("/api/report/workbenchTaskRecord/getModelAcceptanceInfoList", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

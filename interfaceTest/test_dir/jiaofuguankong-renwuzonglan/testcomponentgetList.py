9042baf0642a8c644a6a3947951289cc3fd2e4fb
yxm1004 <1310277501@qq.com>
2023-09-20 16:16:32 +0800

    文件提交

---
+++ interfaceTest/test_dir/jiaofuguankong-renwuzonglan/testcomponentgetList.py
@@ -0,0 +1,25 @@
+import json
+import seldom
+from common.loginApi import Common
+
+class testcomponentgetList(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_componentgetList(self):
+        self.payload = json.dumps({
+            "projectId": "1113",
+            "tableState": 2
+        })
+        self.post("/api/report/component/getList", data=self.payload,headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

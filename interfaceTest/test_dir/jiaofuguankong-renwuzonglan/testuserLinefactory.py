a294a9401ae7384251502d805e30a89237df19fc
yxm1004 <1310277501@qq.com>
2023-09-27 17:35:39 +0800

    提交

---
+++ interfaceTest/test_dir/jiaofuguankong-renwuzonglan/testuserLinefactory.py
@@ -0,0 +1,24 @@
+import json
+import seldom
+from common.loginApi import Common
+class testuserLinefactory(seldom.TestCase)
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_get_userLinefactory(self):
+        """
+           执行者视角-滚动交付-获取用户供货厂商
+        """
+        self.payload = {}
+        self.get("/api/report/userLine/factory", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

d03522cba2b9c25a83fb9f29833cacc4401beb19
yxm1004 <1310277501@qq.com>
2023-11-15 17:40:23 +0800

    提交

---
+++ interfaceTest/test_dir/zhiliangguankong-renwuzonglan/testqueryDeliverInspectionPageOfManage.py
@@ -0,0 +1,34 @@
+import json
+import seldom
+from common.loginApi import Common
+class testqueryDeliverInspectionPageOfManage(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_queryDeliverInspectionPageOfManage(self):
+        """
+           管理者视角-质量管控-查询发货检查
+        """
+        self.payload = json.dumps({
+            "model": {
+                "startTime": "2023-11-01",
+                "endTime": "2023-11-15"
+            },
+            "extra": {},
+            "current": 1,
+            "size": 50
+        })
+        self.post("/api/report/workbenchTaskRecord/queryDeliverInspectionPageOfManage", data=self.payload,
+                  headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)
+

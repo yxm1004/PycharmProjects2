eb31159ffb6661efc95fe6a90df4a8038bad6913
yxm1004 <1310277501@qq.com>
2023-12-19 20:33:34 +0800

    提交

---
+++ interfaceTest/test_dir/yaohuojihuatongji/testupdateDelayReason.py
@@ -0,0 +1,28 @@
+import json
+import seldom
+from common.loginApi import Common
+class testupdateDelayReason(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_updateDelayReason(self):
+        """
+           修改要货计划的延误原因
+        """
+        self.payload = json.dumps([
+            {
+                "id": "1735144059569451008",
+                "delayReason": 0
+            }
+        ])
+        self.post("/api/report/deliveryPlan/updateDelayReason", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

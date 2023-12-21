ca101530d6728bc0c441b8b246826cf89345b57e
yxm1004 <1310277501@qq.com>
2023-12-21 17:28:50 +0800

    提交

---
+++ interfaceTest/test_dir/xiangmulianxiren/testprojectCustomerpage.py
@@ -0,0 +1,31 @@
+import json
+import seldom
+from common.loginApi import Common
+class testprojectCustomerpage(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_projectCustomerpage(self):
+        """
+           项目联系人-分页列表查询
+        """
+        self.payload = json.dumps({
+            "model": {
+                "list": []
+            },
+            "extra": {},
+            "current": 1,
+            "size": 50
+        })
+        self.post("/api/report/projectCustomer/page", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

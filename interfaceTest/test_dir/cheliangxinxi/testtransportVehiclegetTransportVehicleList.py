e24a7ca7bd1354ad56deb33fb2aff1c8e667799c
yxm1004 <1310277501@qq.com>
2023-12-22 10:03:06 +0800

    提交

---
+++ interfaceTest/test_dir/cheliangxinxi/testtransportVehiclegetTransportVehicleList.py
@@ -0,0 +1,25 @@
+import json
+import seldom
+from common.loginApi import Common
+class testtransportVehiclegetTransportVehicleList(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_get_transportVehiclegetTransportVehicleList(self):
+        """
+           获取车牌号
+        """
+        self.payload = {}
+        self.get("/api/report/transportVehicle/getTransportVehicleList?transportVehicle=%E7%B2%A4B331004",
+                 data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

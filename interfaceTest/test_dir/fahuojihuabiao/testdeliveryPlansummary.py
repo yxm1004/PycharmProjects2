f5e96b458d350026252b3895f273919fa24ba898
yxm1004 <1310277501@qq.com>
2023-12-22 15:28:19 +0800

    提交

---
+++ interfaceTest/test_dir/fahuojihuabiao/testdeliveryPlansummary.py
@@ -0,0 +1,24 @@
+import json
+import seldom
+from common.loginApi import Common
+class testdeliveryPlansummary(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_deliveryPlansummary(self):
+        """
+          发货计划-汇总
+        """
+        self.payload = json.dumps({})
+        self.post("/api/report/deliveryPlan/summary", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

491c8a19459081662df702c0828389a7ab2746cb
yxm1004 <1310277501@qq.com>
2023-12-22 09:10:39 +0800

    提交

---
+++ interfaceTest/test_dir/cheliangxinxi/testtransportVehicle2.py
@@ -0,0 +1,34 @@
+import json
+import seldom
+from common.loginApi import Common
+class testtransportVehicle(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_put_transportVehicle(self):
+        """
+           车辆信息-修改
+        """
+        self.payload = json.dumps({
+            "companyName": "东莞物流",
+            "transportVehicle": "粤E123457",
+            "carLength": "13.5",
+            "driverName": "尹晓梅",
+            "driverPhone": "15313487958",
+            "driverName2": "",
+            "driverPhone2": "",
+            "driverName3": "",
+            "driverPhone3": "",
+            "id": "1735468733717377024"
+        })
+        self.put("/api/report/transportVehicle", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "该运输车辆赣D123457已存在顺丰物流公司旗下！"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

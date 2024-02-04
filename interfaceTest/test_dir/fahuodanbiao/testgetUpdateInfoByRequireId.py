ec4556ce26ac0534155a378899a291af27c4cbd0
yxm1004 <1310277501@qq.com>
2024-02-04 14:01:10 +0800

    提交

---
+++ interfaceTest/test_dir/fahuodanbiao/testgetUpdateInfoByRequireId.py
@@ -0,0 +1,28 @@
+import json
+import seldom
+from common.loginApi import Common
+
+class testgetUpdateInfoByRequireId(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_getUpdateInfoByRequireId(self):
+        """
+          发货单-获取项目信息+楼栋
+        """
+        self.payload = json.dumps({
+            "id": "1277",
+            "building": "#B栋"
+        })
+        self.post("/api/report/userLine/factory", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

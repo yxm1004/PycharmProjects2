a225c8bc78c4e4e3ab9a521ee7d8ab503306f659
yxm1004 <1310277501@qq.com>
2024-02-05 14:28:18 +0800

    提交

---
+++ interfaceTest/test_dir/fahuodanbiao/testinvoicecomponent.py
@@ -0,0 +1,48 @@
+import json
+import seldom
+from common.loginApi import Common
+class testinvoicecomponent(seldom.TestCase):
+    def start(self):
+        # 调用登录公共方法构建报文头
+        self.c = Common()
+        self.header = self.c.SetHeader()
+    def test_post_invoicecomponent(self):
+        """
+               发货单构件添加
+        """
+        self.payload = json.dumps({
+            "model": {
+                "list": [
+                    {
+                        "columnFileValue": "1",
+                        "columnFileKey": "scheme_car_number",
+                        "calSymbol": 4,
+                        "dataType": 3
+                    }
+                ],
+                "screenList": [
+                    {
+                        "building": "#B栋",
+                        "floor": "2"
+                    }
+                ],
+                "projectId": "1277",
+                "relateInvoice": True
+            },
+            "extra": {},
+            "current": 1,
+            "size": 50,
+            "order": "ascending,ascending,ascending,ascending",
+            "sort": "building,floor,model,schemeCarNumber"
+        })
+        self.post("/api/report/component/invoice/component", data=self.payload, headers=self.header)
+        self.assertStatusCode(200)
+        assert_data = "成功"  # 断言成功
+        # print("test-----------------"+self.response["msg"])
+        # 取返回msg值断言
+        self.assertJSON(assert_data, self.response["msg"])
+        self.assertJSON(True, self.response["isSuccess"])
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)

ad6a5345f370a379d13194b3a994cf8882031e59
yxm1004 <1310277501@qq.com>
2024-02-06 16:07:32 +0800

    提交

--- interfaceTest/test_dir/huikuanguanli/testreturnedMoneyexportDetails.py
+++ interfaceTest/test_dir/huikuanguanli/testreturnedMoneyexportDetails.py
@@ -19,11 +19,6 @@ class testreturnedMoneyexportDetails(seldom.TestCase):
         })
         self.post("/api/report/returnedMoney/exportDetails", data=self.payload, headers=self.header)
         self.assertStatusCode(200)
-        assert_data = "成功"  # 断言成功
-        # print("test-----------------"+self.response["msg"])
-        # 取返回msg值断言
-        self.assertJSON(assert_data, self.response["msg"])
-        self.assertJSON(True, self.response["isSuccess"])
 
 
 if __name__ == '__main__':
